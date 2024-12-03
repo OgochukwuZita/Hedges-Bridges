6583e3139d57392fdd401bcdd2975cb8c30b3273
Ogbonna Ogochukwu <ogochukwuzita86@gmail.com>
2024-12-03 11:46:33 +0100

    fixed design error

--- hedges-bridges/src/Components/Pointers.jsx
+++ hedges-bridges/src/Components/Pointers.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import '../Styles/About.css'
 import { NavLink } from 'react-router-dom';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
-const Pointers = ({title, to}) => {
+const Pointers = ({ title, to }) => {
   return (
-    <motion.div initial={{color:'#fece', backgroundColor:'#0d151c'}} whileHover={{scale:0.95, backgroundColor:'#a48d07', color:'#0d151c'}} transition={{duration:0.6}} whileTap={{scale:0.95, backgroundColor:'#a48d07', color:'#0d151c'}}  className='exploreCards'>
-        <NavLink to={to}>
+    <motion.div initial={{ color: '#fece', backgroundColor: '#0d151c' }} whileHover={{ scale: 0.95, backgroundColor: '#a48d07', color: '#0d151c' }} transition={{ duration: 0.6 }} whileTap={{ scale: 0.95, backgroundColor: '#a48d07', color: '#0d151c' }} className='exploreCards'>
+      <NavLink to={to}>
         <p>{title}
-          </p>
-        </NavLink>
+        </p>
+      </NavLink>
     </motion.div>
   )
 }
