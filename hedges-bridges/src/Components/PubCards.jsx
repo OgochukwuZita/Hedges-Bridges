422cef2952b864086323854a447e825207a7e027
Ogbonna Ogochukwu <ogochukwuzita86@gmail.com>
2025-01-03 03:57:35 +0100

    publications cards design layout in progress

--- hedges-bridges/src/Components/PubCards.jsx
+++ hedges-bridges/src/Components/PubCards.jsx
@@ -1,63 +1,20 @@
 import React from 'react'
 import '../Styles/publications.css'
+import BreakIcon from '../Components/BreakIcon'
 // import {publications} from '../Components/data'
-
 const PubCards = () => {
   return (
     <div className='pubContainer'>
       <div className='pubCard'>
-      <div className='left'>
-      PUBLICATIONS CARDS GO HERE
-      </div>
-      <div className='right'>
-        <ul>
-          <li>introduction</li>
-          <li>more</li>
+      <div className='introCard'>
+        <h2 className='Title'>This is the publication Title</h2>
+        <ul className='details'>
           <li>author</li>
           <li>date</li>
         </ul>
-      </div>
-      </div>
-      
-      <div className='pubCard'>
-      <div className='left'>
-      PUBLICATIONS CARDS GO HERE
-      </div>
-      <div className='right'>
-        <ul>
-          <li>introduction</li>
-          <li>more</li>
-          <li>author</li>
-          <li>date</li>
-        </ul>
-      </div>
-      </div>
-      
-      <div className='pubCard'>
-      <div className='left'>
-      PUBLICATIONS CARDS GO HERE
-      </div>
-      <div className='right'>
-        <ul>
-          <li>introduction</li>
-          <li>more</li>
-          <li>author</li>
-          <li>date</li>
-        </ul>
-      </div>
-      </div>
-      
-      <div className='pubCard'>
-      <div className='left'>
-      PUBLICATIONS CARDS GO HERE
-      </div>
-      <div className='right'>
-        <ul>
-          <li>introduction</li>
-          <li>more</li>
-          <li>author</li>
-          <li>date</li>
-        </ul>
+        <BreakIcon/>
+        <p>This is the intoductory paragraph</p>
+        <button className='MoreBtn'>Read More</button>
       </div>
       </div>
       
